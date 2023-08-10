8876e7664e211ae75977363995c88cc2f575995f
Tony Ike Ozouli <flawlez18@yahoo.co.uk>
2023-08-09 19:35:13 -0500

    add tf users

---
+++ users/variables.tf
@@ -0,0 +1,17 @@
+variable "username" {
+  type    = list(any)
+  default = ["developer", "manager"]
+}
+
+variable "env" {
+  type    = list(any)
+  default = ["Development", "Production"]
+
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    "Env" = "Production"
+  }
+}
